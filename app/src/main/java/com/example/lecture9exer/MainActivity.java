8a8120bf5a80d81580ed3680e02ea93ae773d233
Modib Qadir <Modibq@gmail.com>
2022-10-07 20:24:03 -0400

    lecture9Exer

---
+++ app/src/main/java/com/example/lecture9exer/MainActivity.java
@@ -0,0 +1,154 @@
+/*
+Mobile App Development I -- COMP.4630 Honor Statement
+The practice of good ethical behavior is essential for
+maintaining good order in the classroom, providing an
+enriching learning experience for students, and
+training as a practicing computing professional upon
+graduation. This practice is manifested in the
+University's Academic Integrity policy. Students are
+expected to strictly avoid academic dishonesty and
+adhere to the Academic Integrity policy as outlined in
+the course catalog. Violations will be dealt with as
+outlined therein. All programming assignments in this
+class are to be done by the student alone unless
+otherwise specified. No outside help is permitted
+except the instructor and approved tutors.
+I certify that the work submitted with this assignment
+is mine and was generated in a manner consistent with
+this document, the course academic policy on the
+course website on Blackboard, and the UMass Lowell
+academic code.
+Date: 10/07/2022
+Name: Modib Qadir
+*/
+package com.example.lecture9exer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ContentValues;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+
+public class MainActivity extends AppCompatActivity {
+
+    private boolean running;
+    private long pauseOffset;
+    private long time;
+    private boolean wasrunning;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button startButton = findViewById(R.id.button);
+        Button pauseButton = findViewById(R.id.button2);
+        Button resetButton = findViewById(R.id.button3);
+        Chronometer chrom = findViewById(R.id.textView);
+
+
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                System.out.println("Start button clicked");
+                if(!running) {
+                    chrom.setBase(SystemClock.elapsedRealtime()-pauseOffset);
+                    running = true;
+                    chrom.start();
+                    time = SystemClock.elapsedRealtime();
+                }
+            }
+        });
+
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                System.out.println("Pause button clicked");
+                if(running) {
+                    chrom.stop();
+                    pauseOffset = SystemClock.elapsedRealtime() - chrom.getBase();
+                    running = false;
+                }
+            }
+
+        });
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                System.out.println("Reset button clicked");
+                chrom.setBase(SystemClock.elapsedRealtime());
+                pauseOffset = 0;
+            }
+        });
+
+        if(savedInstanceState != null){
+            pauseOffset = savedInstanceState.getLong("Offset");
+            running = savedInstanceState.getBoolean("running");
+            time = savedInstanceState.getLong("time");
+            if(running){
+                chrom.setBase(time);
+                chrom.start();
+            }else{
+                chrom.setBase(SystemClock.elapsedRealtime()-pauseOffset);
+            }
+        }
+
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstaceState) {
+        super.onSaveInstanceState(savedInstaceState);
+        savedInstaceState.putLong("Offset", pauseOffset);
+        savedInstaceState.putBoolean("running", running);
+        savedInstaceState.putLong("time", time);
+    }
+
+
+
+    @Override
+    protected  void onStart(){
+        super.onStart();
+        Log.d("STOPWATCH_DEBUG", "in onStart() ");
+    }
+
+    @Override
+    protected  void onResume(){
+        super.onResume();
+        Log.d("STOPWATCH_DEBUG", "in onResume() ");
+    }
+
+    @Override
+    protected  void onRestart(){
+        super.onRestart();
+        Chronometer chrom = findViewById(R.id.textView);
+        Log.d("STOPWATCH_DEBUG", "in onRestart() ");
+
+        if(wasrunning){
+            running = true;
+            chrom.setBase(SystemClock.elapsedRealtime()-pauseOffset);
+            chrom.start();
+        }
+
+    }
+
+    @Override
+    protected  void onPause(){
+
+        super.onPause();
+        Log.d("STOPWATCH_DEBUG", "in onPause() ");
+
+    }
+
+    @Override
+    protected  void onStop(){
+        super.onStop();
+        Log.d("STOPWATCH_DEBUG", "in onStop() ");
+    }
+    @Override
+    protected  void onDestroy(){
+        super.onDestroy();
+        Log.d("STOPWATCH_DEBUG", "in onDestroy() ");
+    }
+}
